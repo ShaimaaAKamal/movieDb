a2d9c900d549baf8f6befd963884238cb09c21a0
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-21 16:36:22 +0200

    add navbar loginOrRegisterComponent to navbar

--- src/app/Components/shared/navbar/create-login-or-rehister-componernt/create-login-or-rehister-componernt.component.ts
+++ src/app/Components/shared/navbar/create-login-or-rehister-componernt/create-login-or-rehister-componernt.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-create-login-or-rehister-componernt',
+  selector: 'app-create-login-or-register',
   standalone: false,
   
   templateUrl: './create-login-or-rehister-componernt.component.html',
