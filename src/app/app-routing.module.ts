2ed2b65a54f61155b3c728dce4708301ee2c9574
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-21 14:12:31 +0200

    add notfound component in the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/MainComponets/home/home.component';
-import { LoginComponent } from './Components/Credientials/login/login.component';
+import { NotFoundComponent } from './Components/Errors/not-found/not-found.component';
 
 const routes: Routes = [
   {path:"",redirectTo:"/home",pathMatch:"full"},
   {path:"home",component:HomeComponent},
-  {path:"**",component:LoginComponent}
+  {path:"**",component:NotFoundComponent}
 ];
 
 @NgModule({
