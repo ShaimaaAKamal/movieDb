03fb2c79c13c88eb75b4d8e64c633316668d7504
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-21 17:13:43 +0200

    start write notification nvabar html code

--- src/app/Components/shared/navbar/logged-user-navbar-section/logged-user-navbar-section.component.ts
+++ src/app/Components/shared/navbar/logged-user-navbar-section/logged-user-navbar-section.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-logged-user-navbar-section',
+  selector: 'app-logged-user-navbar',
   standalone: false,
   
   templateUrl: './logged-user-navbar-section.component.html',
   styleUrl: './logged-user-navbar-section.component.css'
 })
 export class LoggedUserNavbarSectionComponent {
-
+  NoOfNotifications:Number=0;
 }
