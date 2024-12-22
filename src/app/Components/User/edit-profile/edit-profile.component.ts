6ebd32d826e87be59c92a0c3e0c95e4d517d167a
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-22 12:18:54 +0200

    create initial user edit profile  component

---
+++ src/app/Components/User/edit-profile/edit-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-profile',
+  standalone: false,
+  
+  templateUrl: './edit-profile.component.html',
+  styleUrl: './edit-profile.component.css'
+})
+export class EditProfileComponent {
+
+}
