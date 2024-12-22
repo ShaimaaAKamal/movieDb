f874ec014e9839f684edb0479d6c605c3d608bda
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-22 12:16:55 +0200

    create initial user ratings component

---
+++ src/app/Components/User/user-ratings/user-ratings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-ratings',
+  standalone: false,
+  
+  templateUrl: './user-ratings.component.html',
+  styleUrl: './user-ratings.component.css'
+})
+export class UserRatingsComponent {
+
+}
