fb20f100b46446aba591d29ca097fb010a1f9e4d
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-22 12:04:18 +0200

    add navbar user and update styles

--- src/app/Components/shared/navbar/logged-user-navbar-section/navbar-user/navbar-user.component.ts
+++ src/app/Components/shared/navbar/logged-user-navbar-section/navbar-user/navbar-user.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar-user',
   standalone: false,
-  
   templateUrl: './navbar-user.component.html',
   styleUrl: './navbar-user.component.css'
 })
 export class NavbarUserComponent {
-
+User:any={name:"shaimaa kamal"};
+logout(){
+  console.log('logout');
+}
 }
