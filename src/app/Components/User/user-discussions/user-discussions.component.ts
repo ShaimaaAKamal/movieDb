bf90f871bf8c38f042d3857db7529c7cf2abf223
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-22 12:18:29 +0200

    create initial user discussions  component

---
+++ src/app/Components/User/user-discussions/user-discussions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-discussions',
+  standalone: false,
+  
+  templateUrl: './user-discussions.component.html',
+  styleUrl: './user-discussions.component.css'
+})
+export class UserDiscussionsComponent {
+
+}
