c6f025579d9c16768894ec8231dbd87b676b4c0b
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-22 10:22:50 +0200

    create inital navbar notification  account

---
+++ src/app/Components/Components/shared/navbar/navbar-notification/navbar-notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-notification',
+  standalone: false,
+  
+  templateUrl: './navbar-notification.component.html',
+  styleUrl: './navbar-notification.component.css'
+})
+export class NavbarNotificationComponent {
+
+}
