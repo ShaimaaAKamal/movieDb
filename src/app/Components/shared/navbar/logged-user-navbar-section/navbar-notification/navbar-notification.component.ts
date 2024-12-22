77014e297c9bebb94b7f018800ac147041aca3ac
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-22 10:30:43 +0200

    organize navbar file structure and update code

--- src/app/Components/shared/navbar/logged-user-navbar-section/navbar-notification/navbar-notification.component.ts
+++ src/app/Components/shared/navbar/logged-user-navbar-section/navbar-notification/navbar-notification.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-notification',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './navbar-notification.component.css'
 })
 export class NavbarNotificationComponent {
-
+@Input()   NoOfNotifications:Number=0;
 }
