d43328dfc64027ed56b39621c9ff043f6ce78a3e
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-22 12:17:41 +0200

    create initial user lists  component

---
+++ src/app/Components/User/user-lists/user-lists.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-lists',
+  standalone: false,
+  
+  templateUrl: './user-lists.component.html',
+  styleUrl: './user-lists.component.css'
+})
+export class UserListsComponent {
+
+}
