1c99f7515eccdda0cd5de29207c825e88511b3e1
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-22 12:17:18 +0200

    create initial user watchlist  component

---
+++ src/app/Components/User/user-watchlist/user-watchlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-watchlist',
+  standalone: false,
+  
+  templateUrl: './user-watchlist.component.html',
+  styleUrl: './user-watchlist.component.css'
+})
+export class UserWatchlistComponent {
+
+}
