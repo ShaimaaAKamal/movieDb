aabdc6081a734914cf4ed10b27bea9d9fa657d3a
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-23 09:42:17 +0200

    component files structure which chaschanges the imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/shared/navbar/navbar.component';
 import { FooterComponent } from './Components/shared/footer/footer.component';
 import { HomeComponent } from './Components/MainComponets/home/home.component';
-import { MoviesComponent } from './Components/MainComponets/movies/movies.component';
-import { TvShowsComponent } from './Components/MainComponets/tv-shows/tv-shows.component';
+import { MoviesComponent } from './Components/MoviesComponents/movies/movies.component';
+import { TvShowsComponent } from './Components/TVShowsComponents/tv-shows/tv-shows.component';
 import { PeopleComponent } from './Components/MainComponets/people/people.component';
 import { LoginComponent } from './Components/Credientials/login/login.component';
 import { RegisterComponent } from './Components/Credientials/register/register.component';
@@ -19,8 +19,19 @@ import { DiscussionComponent } from './Components/MainComponets/discussion/discu
 import { LeaderboardComponent } from './Components/MainComponets/leaderboard/leaderboard.component';
 import { SupportComponent } from './Components/MainComponets/support/support.component';
 import { NotificationsComponent } from './Components/MainComponets/notifications/notifications.component';
-import { AddNewMovieComponent } from './Components/AddNewComponents/add-new-movie/add-new-movie.component';
-import { AddNewTVShowComponent } from './Components/AddNewComponents/add-new-tvshow/add-new-tvshow.component';
+import { AddNewMovieComponent } from './Components/MoviesComponents/add-new-movie/add-new-movie.component';
+import { AddNewTVShowComponent } from './Components/TVShowsComponents/add-new-tvshow/add-new-tvshow.component';
+import { NavbarUserComponent } from './Components/shared/navbar/logged-user-navbar-section/navbar-user/navbar-user.component';
+import { NavbarNotificationComponent } from './Components/shared/navbar/logged-user-navbar-section/navbar-notification/navbar-notification.component';
+import { ProfileComponent } from './Components/User/profile/profile.component';
+import { UserRatingsComponent } from './Components/User/user-ratings/user-ratings.component';
+import { UserWatchlistComponent } from './Components/User/user-watchlist/user-watchlist.component';
+import { UserListsComponent } from './Components/User/user-lists/user-lists.component';
+import { UserDiscussionsComponent } from './Components/User/user-discussions/user-discussions.component';
+import { EditProfileComponent } from './Components/User/edit-profile/edit-profile.component';
+import { SettingsComponent } from './Components/User/settings/settings.component';
+import { AccountComponent } from './Components/User/account/account.component';
+import { LanguagePrefrenceComponent } from './Components/shared/navbar/language-prefrence/language-prefrence.component';
 
 @NgModule({
   declarations: [
@@ -43,6 +54,17 @@ import { AddNewTVShowComponent } from './Components/AddNewComponents/add-new-tvs
     NotificationsComponent,
     AddNewMovieComponent,
     AddNewTVShowComponent,
+    NavbarUserComponent,
+    NavbarNotificationComponent,
+    ProfileComponent,
+    UserRatingsComponent,
+    UserWatchlistComponent,
+    UserListsComponent,
+    UserDiscussionsComponent,
+    EditProfileComponent,
+    SettingsComponent,
+    AccountComponent,
+    LanguagePrefrenceComponent,
   ],
   imports: [
     BrowserModule,
