e01cdab00f035fe525c3a913fce555b099345fa3
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-23 15:40:42 +0200

    create initail guidelines component

---
+++ src/app/Components/shared/guidelines/guidelines.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guidelines',
+  standalone: false,
+  
+  templateUrl: './guidelines.component.html',
+  styleUrl: './guidelines.component.css'
+})
+export class GuidelinesComponent {
+
+}
