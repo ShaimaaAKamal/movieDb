b0bf345220e179e08feb50d300f71ea66fd982e5
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-23 15:57:54 +0200

    add system status

---
+++ src/app/Components/MainComponets/system-status/system-status.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-system-status',
+  standalone: false,
+  
+  templateUrl: './system-status.component.html',
+  styleUrl: './system-status.component.css'
+})
+export class SystemStatusComponent {
+
+}
