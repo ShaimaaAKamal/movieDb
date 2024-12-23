64254345a064a212317d699199c4f87159bcd33e
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-23 16:06:18 +0200

    create anew api compinet and add api route in routing file

---
+++ src/app/Components/MainComponets/api/api.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api',
+  standalone: false,
+  
+  templateUrl: './api.component.html',
+  styleUrl: './api.component.css'
+})
+export class APIComponent {
+
+}
