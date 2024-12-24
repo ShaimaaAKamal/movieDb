10aa2b4a15d3f9644b15f3657bca2fa417a0d15f
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-24 17:36:41 +0200

    add reactive forms to login components add update html code and add errors message

--- src/app/Components/Credientials/login/login.component.ts
+++ src/app/Components/Credientials/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+LoginForm:FormGroup=new FormGroup({
+    email:new FormControl(null,[Validators.required,Validators.email]),
+    password:new FormControl(null,[Validators.required,Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)]),
+})
+
+ Login(LoginForm:FormGroup){
+
+ }
 }
