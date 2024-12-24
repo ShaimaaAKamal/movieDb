54432948ebb316adb4736bf8d5e7d781234bea84
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-24 17:21:41 +0200

    add clear function and validation

--- src/app/Components/Credientials/register/register.component.ts
+++ src/app/Components/Credientials/register/register.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: false,
-  
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  RegisterForm:FormGroup=new FormGroup({
+    username:new FormControl(null,[Validators.required,Validators.minLength(5),Validators.maxLength(30)]),
+    email:new FormControl(null,[Validators.required,Validators.email]),
+    password:new FormControl(null,[Validators.required,Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)]),
+    confirmPassword:new FormControl(null,[Validators.required,Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)]),
+  })
+
+  SignUP(RegisterForm:FormGroup){
+    
+  }
+  CancalRegisteration(){
+    this.RegisterForm.reset();
+  }
 }
