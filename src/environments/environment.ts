6cc566ef848557a9269331743b2281b16f2328c7
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-24 14:52:30 +0200

    add firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjZkKyQDYgqmnWhfHK0A-XEF3XkfA1S4Q",
-  authDomain: "moviedb-49c93.firebaseapp.com",
-  projectId: "moviedb-49c93",
-  storageBucket: "moviedb-49c93.firebasestorage.app",
-  messagingSenderId: "690054886200",
-  appId: "1:690054886200:web:1cc60597d4dd28004987cd",
-  measurementId: "G-3QMM5K62PK"
+  apiKey: "AIzaSyBLNprxx00fjHwcaozkJ80HaZt9rZKUdWQ",
+  authDomain: "movie-dbko.firebaseapp.com",
+  projectId: "movie-dbko",
+  storageBucket: "movie-dbko.firebasestorage.app",
+  messagingSenderId: "536124947417",
+  appId: "1:536124947417:web:a8c9c3b40a59b2e269f7c8"
 };
 
 export const environment = {
@@ -22,4 +21,4 @@ export const environment = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
