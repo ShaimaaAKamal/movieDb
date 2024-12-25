cdc7dbd07e99caa747ddc8657a94116575295b8e
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-25 15:07:19 +0200

    add authservice to register file to used for user authentication

--- src/app/Components/Credientials/register/register.component.ts
+++ src/app/Components/Credientials/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -8,7 +9,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  constructor(private __AuthService:AuthService){}
   RegisterForm:FormGroup=new FormGroup({
     username:new FormControl(null,[Validators.required,Validators.minLength(5),Validators.maxLength(30)]),
     email:new FormControl(null,[Validators.required,Validators.email]),
@@ -17,7 +18,12 @@ export class RegisterComponent {
   })
 
   SignUP(RegisterForm:FormGroup){
-    
+    let userEmail:string=(RegisterForm.get('email')?.value);
+    let userPassword=RegisterForm.get('password')?.value;
+    this.__AuthService.signUp(userEmail,userPassword, RegisterForm.get('username')?.value).subscribe({
+      next:()=> console.log('registerted'),
+      error: (err) => console.error('Registration error:', err),
+    })
   }
   CancalRegisteration(){
     this.RegisterForm.reset();
