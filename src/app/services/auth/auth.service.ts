b24fa1b4cd33d5dc06037a4b7d3d48b78f5a92df
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-28 15:34:21 +0200

    add signup methof to authservice and create user behaviour subject to be used arround components to define the displayed routes depending on the guard

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -3,13 +3,18 @@ import { BehaviorSubject, Observable, from, throwError } from 'rxjs';
 import { catchError, switchMap } from 'rxjs/operators';
 import { User } from '../../interfaces/user';
 import { FirebaseService } from '../firebase/firebase.service';
+import { Router } from '@angular/router';
+import { HelpersService } from '../helpers/helpers.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   token=new BehaviorSubject('');
-  constructor(private firebaseService: FirebaseService) {}
+  user:any=new BehaviorSubject(null);
+  constructor(private firebaseService: FirebaseService,private __Router:Router,private __Helpers:HelpersService) {
+    this.user.next(this.__Helpers.getUserDataFromlocalStoarge());
+  }
 
   signUp(email: string, password: string, username: string): Observable<void> {
     return from(this.firebaseService.signUp(email, password)).pipe(
@@ -20,6 +25,7 @@ export class AuthService {
           return from(this.firebaseService.setUserData(user.uid, {
             email: user.email || '',
             username,
+            uid:user.uid,
             // Avoid storing passwords in the database
           }));
         } else {
@@ -54,5 +60,20 @@ export class AuthService {
         return throwError(() => error);
       })
     );
+  }
+
+  Logout(): void {
+    this.firebaseService.logout().subscribe({
+      next: () => {
+        console.log('in');
+        localStorage.removeItem('token');
+        localStorage.removeItem('userData');
+        this.user.next(null);
+        this.__Router.navigate(['Login']);
+      },
+      error: (err) => {
+        console.error('Logout error:', err);
+      },
+    });
   }
 }
