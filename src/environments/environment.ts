37e648c84d17cea4a019475df31aafc3678b7998
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-28 11:10:07 +0200

    update file to only have firebase configration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFirestore, doc, setDoc } from "firebase/firestore";
-
 const firebaseConfig = {
   apiKey: "AIzaSyBLNprxx00fjHwcaozkJ80HaZt9rZKUdWQ",
   authDomain: "movie-dbko.firebaseapp.com",
@@ -16,24 +11,4 @@ export const environment = {
   production: false,
   firebase: firebaseConfig
 };
-
-
-
-// Initialize Firebase services
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore();
-
-onAuthStateChanged(auth, async (user) => {
-  if (user) {
-    const userRef = doc(db, "users", user.uid);
-    await setDoc(userRef, {
-      email: user.email,
-      displayName: user.displayName,
-      createdAt: new Date().toISOString()
-    });
-  }
-});
-
 
