068ce09029bac29bb9b7ee039f9ef2b8608d8981
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-28 11:34:43 +0200

    show routing after logged

--- src/app/Components/Credientials/login/login.component.ts
+++ src/app/Components/Credientials/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup,FormControl,Validators} from '@angular/forms';
-
+import { AuthService } from '../../../services/auth/auth.service';
+import { HelpersService } from '../../../services/helpers/helpers.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: false,
@@ -9,13 +11,31 @@ import { FormGroup,FormControl,Validators} from '@angular/forms';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+LoginFailed:boolean=false;
+constructor(private __AuthService:AuthService,private __HelpersService:HelpersService,private __Router:Router){}
 LoginForm:FormGroup=new FormGroup({
     email:new FormControl(null,[Validators.required,Validators.email]),
     password:new FormControl(null,[Validators.required,Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)]),
 })
 
- Login(LoginForm:FormGroup){
 
- }
-}
+
+ Login(LoginForm:FormGroup){
+    this.__AuthService.signIn(LoginForm.get('email')?.value,LoginForm.get('password')?.value).subscribe({
+      next: (userData:any)=>{
+        this.LoginFailed=false;
+        this.__HelpersService.setUserDataTolocalStoarge(userData);
+        this.__AuthService.token.subscribe({
+          next:(token)=>{
+             this.__HelpersService.setTokenTolocalStoarge(token);
+             this.__Router.navigate(['/Home']);
+          }
+        })
+      },
+      error:()=>{this.LoginFailed=true;},
+    });
+  }
+  
+  }
+
+
