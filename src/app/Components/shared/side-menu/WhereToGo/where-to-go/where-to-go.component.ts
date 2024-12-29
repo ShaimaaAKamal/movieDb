10ea9a873f6dd2cd5a0f7a4744ba641218745b0f
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2024-12-29 11:02:45 +0200

    create initial component where to go component to side menu

---
+++ src/app/Components/shared/side-menu/WhereToGo/where-to-go/where-to-go.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-where-to-go',
+  standalone: false,
+  
+  templateUrl: './where-to-go.component.html',
+  styleUrl: './where-to-go.component.css'
+})
+export class WhereToGoComponent {
+
+}
